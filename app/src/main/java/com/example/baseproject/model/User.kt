f8273aca5d1bb09ac5c19410ca7e64a41229b6dc
Leinad745 <da.corderom@duocuc.cl>
2025-11-12 19:16:26 -0300

    Implementacion de foto de perfil

--- app/src/main/java/com/example/baseproject/model/User.kt
+++ app/src/main/java/com/example/baseproject/model/User.kt
@@ -1,9 +1,12 @@
 package com.example.baseproject.model
 
+import android.net.Uri
+
 data class Usuario(
     val mail: String,
     val nombreUsuario: String,
     val contrasena: String,
     val createdAT: Long = System.currentTimeMillis(),
-    val favoritos: List<String>) {
+    val favoritos: List<String>,
+    val imagenUri: Uri? = null) {
 }
