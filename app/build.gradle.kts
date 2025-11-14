6e4dc5e9197986dffc1819cf5741fe7622ba87dd
Leinad745 <da.corderom@duocuc.cl>
2025-11-14 15:56:30 -0300

    creacion de vista de registro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
 
     // Testing dependencies
     testImplementation("junit:junit:4.13.2")
